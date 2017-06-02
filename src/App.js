bbc0f8fafd4f108140fc5c646d27c850578c2cb6
erinknight242 <erinknight242@gmail.com>
2017-06-01 23:31:39 -0500

    Tie auth to bowling data, add signout

--- src/App.js
+++ src/App.js
@@ -1,78 +1,92 @@
 import React, { Component } from 'react';
-import Layout from './Layout';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
-import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as firebase from 'firebase';
+import NewUser from './NewUser.jsx';
+import KnownUser from './KnownUser.jsx';
+import { getAverage } from './helpers';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+require('../assets/css/style.scss');
 
-const styles = {
-  container: {
-    textAlign: 'center',
-    marginTop: 20
-  },
-};
-
-const muiTheme = getMuiTheme({
-  palette: {
-    accent1Color: deepOrange500,
-  },
-});
+injectTapEventPlugin();
 
 export default class App extends Component {
     constructor(props, context) {
     super(props, context);
 
-    this.handleRequestClose = this.handleRequestClose.bind(this);
-    this.handleTouchTap = this.handleTouchTap.bind(this);
+    this.getGames = this.getGames.bind(this);
+
+    // Initialize Firebase
+    var config = {
+      apiKey: 'AIzaSyBOBjNLRhnXM4vLv4zVfCF9_yFt5V0C1EI',
+      authDomain: 'bowlingaverage-e9ad3.firebaseapp.com',
+      databaseURL: 'https://bowlingaverage-e9ad3.firebaseio.com',
+      projectId: 'bowlingaverage-e9ad3',
+      storageBucket: 'bowlingaverage-e9ad3.appspot.com',
+      messagingSenderId: '994230171423'
+    };
+    firebase.initializeApp(config);
 
     this.state = {
-      open: false,
+      uid: null,
+      db: firebase.database(),
+      games: [],
+      loading: true
     };
   }
 
-  handleRequestClose() {
-    this.setState({
-      open: false,
+  componentDidMount() {
+    var me = this;
+    firebase.auth().onAuthStateChanged(function(user) {
+      if (user) {
+        // User is signed in.
+        var displayName = user.displayName;
+        var email = user.email;
+        var emailVerified = user.emailVerified;
+        var photoURL = user.photoURL;
+        var uid = user.uid;
+        var phoneNumber = user.phoneNumber;
+        var providerData = user.providerData;
+        user.getIdToken().then(function(accessToken) {
+            me.setState({ uid });
+            me.getGames(uid);
+            }, null, '  ');
+        } else {
+        // User is signed out.
+        window.location.replace('http://localhost:3000');
+        }
+      }, function(error) {
+        console.log(error);
     });
   }
 
-  handleTouchTap() {
-    this.setState({
-      open: true,
+  getGames(uid) {
+    var me = this;
+    const ref = this.state.db.ref('users').orderByChild('uid').equalTo(uid).on('value', function(snapshot) {
+      var data = snapshot.val();
+      console.log(data);
+      if (data) {
+        me.setState({
+          games: data[0].games,
+          average: getAverage(data[0].games),
+          loading: false
+        });
+      } else {
+        me.setState({ loading: false });
+      }
+    }, function (errorObject) {
+      console.log('The read failed: ' + errorObject.code);
     });
+  }
+
+  signOut() {
+    firebase.auth().signOut();
   }
 
   render() {
-    const standardActions = (
-      <FlatButton
-        label="Ok"
-        primary={true}
-        onClick={this.handleRequestClose}
-      />
-    );
+    const loading = <div></div>;
 
-    return (
-      <Layout>
-        <MuiThemeProvider muiTheme={muiTheme}>
-            <div style={styles.container}>
-            <Dialog
-                open={this.state.open}
-                title="Super Secret Password"
-                actions={standardActions}
-                onRequestClose={this.handleRequestClose}
-            >
-                1-2-3-4-5
-            </Dialog>
-            <RaisedButton
-                label="Super Secret Password"
-                secondary={true}
-                onClick={this.handleTouchTap}
-            />
-            </div>
-        </MuiThemeProvider>
-      </Layout>
-    );
+    const app = this.state.uid != null && this.state.games.length ? 
+      <KnownUser average={this.state.average} signOut={this.signOut} /> : <NewUser signOut={this.signOut} />;
+
+    return this.state.uid != null && !this.state.loading ? app : loading;
   }
 }
